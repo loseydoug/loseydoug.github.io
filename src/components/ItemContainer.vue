3c169645dace3a07ce74b776f6e22ea15657654f
Doug Losey <dlosey@wsgc.com>
2021-09-06 14:47:23 -0400

    add pokedex base

---
+++ src/components/ItemContainer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div @click="toggleExpand">{{ item.name.english }}
+      <ItemPanel :item="item" :expanded="expanded" />
+    </div>
+  </div>
+</template>
+<script>
+import ItemPanel from "./ItemPanel.vue"
+export default {
+  name: "itemContainer",
+  components: {
+    ItemPanel,
+  },
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      expanded: false
+    } 
+  },
+  mounted() {
+    this.item.expanded = false;
+  },
+  methods: {
+    toggleExpand: function() {
+      console.log('here');
+      this.expanded = !this.expanded;
+    }
+  },
+}
+</script>
---
+++ src/views/Pokedex.vue
@@ -0,0 +1,14 @@
+<template>
+  <PokedexContainer />
+</template>
+
+<script>
+import PokedexContainer from "@/components/PokedexContainer.vue";
+
+export default {
+  name: "PokedexView",
+  components: {
+    PokedexContainer,
+  },
+};
+</script>
