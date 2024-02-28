a1b5a159e34b8ae161a1b1dc41036ea956edde38
Doug <losey.doug@gmail.com>
2024-02-28 16:59:21 -0500

    add bakery 2]

--- src/components/BakingContainer.vue
+++ src/components/BakingContainer.vue
@@ -2,9 +2,9 @@
   <div class="bakery-container">
     <h1>Welcome to the Bakery!</h1>
     <div v-for="item in menu" :key="item.id">
-      <h2>${item.name}</h2>
-      <p v-if="item.flavors">Flavors: ${item.flavors.join(", ")}</p>
-      <h3>${item.price}</h3>
+      <h2>{{item.name}}</h2>
+      <p v-if="item.flavors">Flavors: {{item.flavors.join(", ")}}</p>
+      <h3>{{item.price}}</h3>
     </div>
   </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -13,11 +13,14 @@ const routes = [
   {
     path: "/Pokedex",
     name: "PokedexView",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/Pokedex.vue"),
+  },
+  {
+    path: "/Bakery",
+    name: "BakingPage",
+    component: () =>
+      import(/* webpackChunkName: "baking" */ "../views/Baking.vue"),
   },
 ];
 
